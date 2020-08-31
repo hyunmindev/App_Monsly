14667adf92d1cf850bd9d5647ccdcc94b90ce572
hyunmindev <jung@hyunmin.dev>
2020-08-31 20:01:01 +0900

    Initial Commit

---
+++ Monsly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Monsly
+//
+//  Created by 정현민 on 2020/08/31.
+//  Copyright © 2020 Hyunmin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
